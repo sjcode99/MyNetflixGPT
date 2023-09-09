b603b3010f6928c903ff398d91f35c7b6a499142
sjcode99 <misuraj2611@gmail.com>
2023-09-09 17:35:02 +0530

    scrollbar bug fixed

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
   return (
-    <div className="px-6 bg-black">
+    <div className="px-6">
       <h1 className="text-3xl text-white py-4">{title}</h1>
       <div className="flex overflow-x-scroll">
         <div className="flex">
